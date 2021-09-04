074b267b9150deff5be5962c7219a011b54cc997
FixedC4ke <drmixe@gmail.com>
2021-09-04 23:33:54 +0500

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Button text="test"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+  padding: 0;
+}
+.container {
+  width: 980px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="navbar">
+        <div class="container">
+            <header class="header">
+                <a href="/"><img src="#" alt="Logo"></a>
+                <nav class="links">
+                    <ul class="nav-links">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/">Главная страница</a>
+                        </li>
+                            <li class="nav-item">
+                            <a class="nav-link"  href="/login">Войти</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    .header{
+        display: flex;
+        justify-content: space-between;
+        height: 50px;
+        line-height: 50px;
+    }
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+    .nav-item{
+        display: inline-block;
+        margin: 0 10px;
+    }
+    .navbar{
+        background-color: black;
+        color: white;
+    }
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    .nav-link:hover{
+        color: grey;
+    }
+</style>
